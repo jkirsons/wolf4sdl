fe2d8c69d10cc61264c73d6514ff0dadb6ed0e00
Jason <jkirsons@gmail.com>
2019-01-14 20:23:11 +0100

    Spear of Destiny

--- build_release.sh
+++ build_release.sh
@@ -1,11 +1,14 @@
 make clean
-make -j2 EXTRA_CPPFLAGS='-DVERSIONALREADYCHOSEN=1 -DGOODTIMES=1 -DCARMACIZED=1 -DDATADIR="\"/sd/data/Wolfenstein 3D/base/\"" -DPLAYDEMOLIKEORIGINAL=1 -DDEMOSEXTERN=1 -DARTSEXTERN=1'
-#cd release
+make -j2 EXTRA_CPPFLAGS='-DVERSIONALREADYCHOSEN=1 -DGOODTIMES=1 -DCARMACIZED=1 -DSPEAR=1 -DDATADIR="\"/sd/data/Wolfenstein 3D/base/\"" -DPLAYDEMOLIKEORIGINAL=1 -DDEMOSEXTERN=1 -DARTSEXTERN=1'
 /Applications/ffmpeg -i ./images/Tile.png -f rawvideo -pix_fmt rgb565 ./images/tile.raw
-#~/Documents/GitHub/odroid-go-firmware/tools/mkfw/mkfw Duke3D ./images/tile.raw 0 16 1048576 app ./build/Duke3D.bin
-#rm Duke3D-snd.fw
-#mv firmware.fw Duke3D-snd.fw
-
 ~/Documents/GitHub/odroid-go-firmware-20181001/tools/mkfw/mkfw Wolf3D ./images/tile.raw 0 16 1048576 app ./build/Wolf3D.bin
 rm ./release/Wolf3D.fw
 mv firmware.fw ./release/Wolf3D.fw
+
+
+make clean
+make -j2 EXTRA_CPPFLAGS='-DVERSIONALREADYCHOSEN=1 -DGOODTIMES=1 -DCARMACIZED=1 -DDATADIR="\"/sd/data/Spear Of Destiny/base/\"" -DPLAYDEMOLIKEORIGINAL=1 -DDEMOSEXTERN=1 -DARTSEXTERN=1'
+/Applications/ffmpeg -i ./images/TileSOD.png -f rawvideo -pix_fmt rgb565 ./images/tileSOD.raw
+~/Documents/GitHub/odroid-go-firmware-20181001/tools/mkfw/mkfw SpearOfDestiny ./images/tileSOD.raw 0 16 1048576 app ./build/Wolf3D.bin
+rm ./release/Spear\ Of\ Destiny.fw
+mv firmware.fw ./release/Spear\ Of\ Destiny.fw
